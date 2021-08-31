ffd6053832892c615a84e36da9c7310a3c24b917
FarahAljasser <76225517+FarahAljasser@users.noreply.github.com>
2021-08-31 10:48:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StudentsGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string countinue = "Yes";
+
+            while (countinue == "Yes") {
+                Student students = new Student { };
+
+                Console.WriteLine("Please Enter The Name: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Please Enter Your Grade: ");
+                string grade = Console.ReadLine();
+
+                Console.WriteLine(students + " is " + students.passedOrNot);
+
+                Console.WriteLine("Do You Add More? ");
+                countinue = Console.ReadLine().ToUpper();
+            }
+        }
+    }
+
+    class Student {
+        public string name { get; set; }
+        public int grade { get; set; }
+        public string passedOrNot { get { string reasult = (grade <= 60) ? "Failed " : "Passed "; return reasult; } }
+    }
+
+    
+}
